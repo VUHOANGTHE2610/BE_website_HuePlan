816ef53e6ad8982763996d5921cae3e8bfcb822d
VUHOANGTHE2610 <hoangthevu8910@gmail.com>
2025-03-28 23:50:09 +0700

    folder configuration and organization

---
+++ src/main/java/com/vuhoang/hueplan/HuePlanApplication.java
@@ -0,0 +1,13 @@
+package com.vuhoang.hueplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuePlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuePlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vuhoang/hueplan/HuePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuhoang.hueplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuePlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
